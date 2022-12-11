b38a7bd01ae98aa76452db55b3849d985f5ffa0d
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-12-11 08:05:16 +0500

    Lecture 3
    String and intro to Lists

---
+++ 01-PythonIntro/classpractice.py
@@ -0,0 +1,16 @@
+# print(1+1)
+# print(len("This is Jahanzeb's pen"))
+# print("Naeem")
+# fName = "Syed"
+# lName = "Nauman"
+# s = "This is Jahanzeb's pen"
+# print("Welcome to the lecture {l} {f}, hope you are learning something".format(
+# f=fName, l=lName))
+
+# my_list = ['one', 'two', 'three', 4, 5]
+# print(my_list)
+# my_list = my_list + ['new item']
+# print(my_list)
+
+my_list = [1, 2, 3]
+print(my_list * 3)
